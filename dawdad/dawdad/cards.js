900beaf2faac5407be5c22d5ca752fd1c535de4c
gsm1020 <43049070+gsm1020@users.noreply.github.com>
2019-01-26 11:53:58 -0500

    asdafgf

---
+++ dawdad/dawdad/cards.js
@@ -0,0 +1,29 @@
+var deck = ["2 Heart", "3 Heart", "4 Heart", "5 Heart",
+            "2 Spade", "3 Spade", "4 Spade", "5 Spade",
+            "2 Club", "3 Club", "4 Club", "5 Club",
+            "2 Diamond", "3 Diamond", "4 Diamond", "5 Diamond"];
+
+var chosenCards = [];
+
+
+function shuffleDeck(array) {
+    array = chosenCards;
+    array.sort(function(a, b) {return 0.5 - Math.random()});
+    return array
+}
+
+
+function drawCard() {
+    var randomNum = Math.floor(Math.random() * deck.length);
+    var card = deck[randomNum];
+    deck.splice(randomNum, 1);
+    if (deck.length === 0) {
+        deck = chosenCards;
+        chosenCards = []
+        return console.log("cards shuffled, draw again.");
+    } else {
+        chosenCards.unshift(card);
+        return console.log(chosenCards[0]);
+    }
+}
+
